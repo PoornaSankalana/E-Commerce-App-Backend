8c777f349b787262cc0dd01e672ccae97e153f81
Poorna Sankalana <sanka2016pps@gmail.com>
2022-07-10 13:48:44 +0530

    Order Model

---
+++ src/api/models/Order.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+    userId: { type: String, required: true, unique: true },
+    products: [
+        {
+            productId: { type: String },
+            quantity: { type: Number, default: 1 },
+        },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
