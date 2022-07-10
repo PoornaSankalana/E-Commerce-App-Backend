416697e0c624e1990747c16f7c6efb9d8404a076
Poorna Sankalana <sanka2016pps@gmail.com>
2022-07-10 13:48:15 +0530

    main runnable file

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRouter = require("./api/routes/userRoute");
+const authRouter = require("./api/routes/auth");
+const productRoutes = require("./api/routes/productRoutes");
+
+const app = express();
+app.use(express.json());
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("DB Connection Successfull !"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Backend server is running !`);
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/products", productRoutes);
